ae8e9d85abf957c1f27b0829111938e1582a26aa
AJ ONeal <awesome@coolaj86.com>
2016-08-01 10:35:00 -0400

    preserve demo.json remove ursa (compiled)

--- ZIP.bash
+++ ZIP.bash
@@ -13,7 +13,15 @@ rsync -av --exclude=.git --exclude=test ./ /tmp/pokemap-build/pokemap/
 pushd /tmp/pokemap-build/
   pushd /tmp/pokemap-build/pokemap/
     rm -f ZIP.bash
+
+    # preserve demo.json, but remove other debug files
+    rsync -av utils/demo.json ./
     rm -rf utils/
+    mkdir -p utils/
+    mv demo.json utils/
+
+    # remove ursa and rely only on node-forge
+    rm -rf node_modules/ursa
 
     pushd public/
       rm -f Dockerfile Gruntfile.js Procfile app.json package.json requirements.txt runserver.py
